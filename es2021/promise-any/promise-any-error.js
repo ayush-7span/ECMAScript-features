5291e9123e7dd7ed368d72993034a2c4de68fab3
Ayush Ghetia <107481889+ayush-7span@users.noreply.github.com>
2023-07-24 17:38:34 +0530

    initial commit

---
+++ es2021/promise-any/promise-any-error.js
@@ -0,0 +1,12 @@
+(async () => {
+    try {
+        const output = await Promise.any([
+          Promise.reject('Error 1'),
+          Promise.reject('Error 2'),
+          Promise.reject('Error 3'),
+      ]);
+      console.log(`Output: ${output}`);
+    } catch (err) {
+        console.log(`Error: ${err.errors}`);
+    }
+  })(); 
